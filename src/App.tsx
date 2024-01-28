18941c109b1dc847a0aeda9ee0f9a1fac16adf6d
nittosantos <nittosantos.dev@gmail.com>
2024-01-28 16:55:57 -0300

    feat: :fire:

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import './global.css'
-
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Toaster position="top-right" richColors />
-      <Helmet titleTemplate="%s | Pizza Shop" />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
 
-      <RouterProvider router={router} />
+        <Toaster richColors />
+
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
