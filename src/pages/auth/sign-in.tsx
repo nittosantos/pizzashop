cda4fcfbedd951bcebe1a6dc37d8e7fd96199ea6
Nitto <nittosantos.dev@gmail.com>
2024-02-06 11:55:01 -0300

    feat: :sparkles: integrated sign-up to back end

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -17,20 +17,26 @@ const signInForm = z.object({
 type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+  const [searchParams] = useSearchParams()
+
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignInForm>()
+  } = useForm<SignInForm>({
+    defaultValues: {
+      email: searchParams.get('email') ?? '',
+    },
+  })
 
-  const { mutateAsync: authenticate } = useMutation({
+  const { mutateAsync: authenticateFn } = useMutation({
     mutationFn: signIn,
   })
 
   async function handleSignIn(data: SignInForm) {
     try {
 
-      await authenticate({ email: data.email })
+      await authenticateFn({ email: data.email })
       toast.success('Enviamos um link de autenticação para seu e-mail')
     } catch {
       toast.error('Credenciais invalidas')
