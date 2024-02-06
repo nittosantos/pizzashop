10959ebd4855dc724a92a7f046ec8eb68f39bc06
Nitto <nittosantos.dev@gmail.com>
2024-02-06 16:59:53 -0300

    feat: :sparkles: invalidat query to page

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,20 @@
 import './global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
-import { router } from './routes'
-import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './lib/react-query'
+import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
         <Helmet titleTemplate="%s | Pizza Shop" />
-        <Toaster position="top-right" richColors />
+        <Toaster richColors />
 
         <QueryClientProvider client={queryClient}>
           <RouterProvider router={router} />
