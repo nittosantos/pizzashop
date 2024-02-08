f204506711da9d582acab5dab2fb47124ba7b4e4
Nitto <nittosantos.dev@gmail.com>
2024-02-08 14:54:26 -0300

    feat: :sparkles: add a loading state from metrics

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,6 @@
-import { env } from '@/env'
 import axios from 'axios'
+
+import { env } from '@/env'
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
@@ -7,8 +8,10 @@ export const api = axios.create({
 })
 
 if (env.VITE_ENABLE_API_DELAY) {
-  api.interceptors.request.use(async (config ) => {
-    await new Promise(resolve => setTimeout(resolve, 2000))
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) =>
+      setTimeout(resolve, Math.round(Math.random() * 3000)),
+    )
     return config
   })
 }
