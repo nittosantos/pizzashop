6dc659e1cb68ef23f3d2bbae2a58289661ee4426
Nitto <nittosantos.dev@gmail.com>
2024-02-08 14:57:15 -0300

    fix: :bug:

--- src/pages/app/dashboard/metric-card-skeleton.tsx
+++ src/pages/app/dashboard/metric-card-skeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '@/components/ui/skeleton'
 export function MetricCardSkeleton() {
   return (
     <>
-      <Skeleton className="mt-1 h-7 w-36" />
+      <Skeleton className="mt-1 h-7 w-24" />
       <Skeleton className="h-4 w-52" />
     </>
   )
